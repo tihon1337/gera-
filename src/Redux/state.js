72e1aebee0a2106b680ea0b5f28cd6fe92e9c7dc
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2021-10-18 20:22:51 +0300

    9

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,47 +1,59 @@
-import {renderEntireTree} from "../render";
-
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi a new massage', likesCount: 12},
-            {id: 2, message: 'This seconde ange', likesCount: 13},
-            {id: 3, message: 'This seconde mess', likesCount: 16},
-            {id: 4, message: 'This seconde ', likesCount: 18},
-            {id: 5, message: 'This secon', likesCount: 9},
-        ],
-        newPostText: 'it-kamasutra.com',
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi a new massage', likesCount: 12},
+                {id: 2, message: 'This seconde ange', likesCount: 13},
+                {id: 3, message: 'This seconde mess', likesCount: 16},
+                {id: 4, message: 'This seconde ', likesCount: 18},
+                {id: 5, message: 'This secon', likesCount: 9},
+            ],
+            newPostText: 'it-kamasutra.com',
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'},
+            ],
+            messages: [{id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you'},
+                {id: 3, message: 'yo'},
+                {id: 4, message: 'yo'},
+                {id: 5, message: 'yo'},
+                {id: 6, message: 'yo'},
+            ],
+        },
     },
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'},
-        ],
-        messages: [{id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you'},
-            {id: 3, message: 'yo'},
-            {id: 4, message: 'yo'},
-            {id: 5, message: 'yo'},
-            {id: 6, message: 'yo'},
-        ],
+    _callSubscriber() {
+        console.log('State changed');
     },
-}
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText='';
-    renderEntireTree(state);
-}
-export let updateNewPostText = (newText) => {
+    getState() {
+        return this._state
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
 
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observe) {
+        this._callSubscriber = observe;
+
+    }
+
 }
-export default state;
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import store from './Redux/state';
+import App from "./App";
 //import * as serviceWorker from './serviceWorker'
-import {renderEntireTree} from "./render";
-import state from "./Redux/state";
+import {BrowserRouter} from "react-router-dom";
 
-renderEntireTree(state);
+
+let renderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
+        </BrowserRouter>, document.getElementById("root"));
+}
+
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
 //serviceWorker.unregister();
