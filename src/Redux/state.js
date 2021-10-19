e8124afffa3b9c8346a144ee2ee36f04a606195e
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2021-10-20 01:21:09 +0300

    11

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -38,26 +38,19 @@ let store = {
         this._callSubscriber = observe;
 
     },
-    _addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    },
-    _updateNewPostText(newText) {
-
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
-    },
     dispatch(action) {
         if (action.type === 'ADD-POST'){
-            this._addPost()
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0,
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
         }else if(action.type ==='UPDATE-NEW-POST-TEXT'){
-            this._updateNewPostText(action.newText)
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
         }
     }
 
