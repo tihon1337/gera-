70b10ecf7478825c73d147d9e0cdd9efbeafbb3c
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2021-10-26 21:20:04 +0300

    (16) добавляем redux

--- src/Redux/origenal-redux/redux-store.js
+++ src/Redux/origenal-redux/redux-store.js
@@ -1,7 +1,7 @@
 import {combineReducers, createStore} from "redux";
-import profileReducer from "./profile-reducer";
-import dialogsReducer from "./dialogs-reducer";
-import sidebarReducer from "./sidebar-reducer";
+import profileReducer from "../profile-reducer";
+import dialogsReducer from "../dialogs-reducer";
+import sidebarReducer from "../sidebar-reducer";
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import store from './Redux/redux-store';
+import store from './Redux/origenal-redux/redux-store';
 import App from "./App";
 //import * as serviceWorker from './serviceWorker'
 import {BrowserRouter} from "react-router-dom";
