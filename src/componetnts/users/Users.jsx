e0b60c0669601856e8b5fda6793f7f5178deb091
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2021-11-10 22:10:17 +0300

    (26)  классовый компонент (extends React.Component)

--- src/componetnts/users/Users.jsx
+++ src/componetnts/users/Users.jsx
@@ -3,27 +3,22 @@ import styles from "./styles.module.css"
 import * as axios from "axios";
 import userPhoto from '../../assets/94-943097_stockvader-predicted-adig-user-profile-image-png.png'
 
-let Users = (props) => {
-
-    let getUsers = () =>
-    {
-        if (props.users.length === 0) {
+class Users extends React.Component {
+    getUsers = () => {
+        if (this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
-                props.setUsers(response.data.items)
-            });
+                    this.props.setUsers(response.data.items)
+                });
         }
-        /* props.setUsers([
-                 {id: 1, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/3491078/pub_5ed3717ae1c2261710b4e3c8_5ed3914b932a8736ef34a981/scale_1200', followed: false, fullName: 'Dimitry', status: 'I am a boss', location: {city: ' Moscow', country: 'Russia'}},
-                 {id: 2, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/3491078/pub_5ed3717ae1c2261710b4e3c8_5ed3914b932a8736ef34a981/scale_1200', followed: true, fullName: 'Boyan', status: 'I am from Serbia', location: {city: ' Belgrade', country: 'Serbia'}},
-                 {id: 3, photoUrl: 'https://avatars.mds.yandex.net/get-zen_doc/3491078/pub_5ed3717ae1c2261710b4e3c8_5ed3914b932a8736ef34a981/scale_1200', followed: false, fullName: 'Tihon', status: 'hero', location: {city: ' Donetsk', country: 'DPR'}},
-             ]*/
     }
 
-    return <div>
-        <button onclick={ getUsers}>Get Users</button>
-        {
-            props.users.map(u => <div key={u.id}>
+
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
@@ -33,14 +28,14 @@ let Users = (props) => {
                     {u.followed
 
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                            this.props.unfollow(u.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            this.props.follow(u.id)
                         }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -51,9 +46,11 @@ let Users = (props) => {
                 </span>
                 </span>
 
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 
 export default Users;
