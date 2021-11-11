c8a7be8772f9888f39a494a64044c791f3542e5f
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2021-11-11 22:22:17 +0300

    (27)  жизненный цикл, life cycle methods, componentDidMount

--- src/componetnts/users/Users.jsx
+++ src/componetnts/users/Users.jsx
@@ -4,8 +4,8 @@ import * as axios from "axios";
 import userPhoto from '../../assets/94-943097_stockvader-predicted-adig-user-profile-image-png.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
+
         if (this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
