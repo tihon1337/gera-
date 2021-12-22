382d30ef94d2f889325b5d148ed46a1b0e7e95c3
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2021-12-22 22:44:54 +0300

    (41) High Order Component (hoc)

--- src/componetnts/profile/ProfileContainer.jsx
+++ src/componetnts/profile/ProfileContainer.jsx
@@ -5,6 +5,7 @@ import {connect} from "react-redux";
 import {getUserProfile, setUserProfile} from "../../Redux/profile-reducer";
 import {Redirect, withRouter} from "react-router-dom";
 import {usersAPI} from "../../API/API";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 
 class ProfileContainer extends React.Component{
@@ -17,17 +18,22 @@ class ProfileContainer extends React.Component{
    }
 
     render() {
-       if (!this.props.isAuth) return <Redirect to='login'/>
+
 
         return (
             <Profile {...this.props} profile={this.props.profile}/>
         );
     }
 }
+
+
+
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer)
+
 let mapStateToProps =(state)=>({
     profile: state.profilePage.profile,
-    isAuth: state.auth.isAuth
 })
 
-let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent);
+
 export default connect(mapStateToProps,{getUserProfile})(WithUrlDataContainerComponent,ProfileContainer);
