98c5f02016794f581855690a9454cc449461c483
tihon1337 <75581326+tihon1337@users.noreply.github.com>
2022-01-10 21:46:30 +0300

    (43.1)  setState, local state

--- src/componetnts/profile/ProfileleInfo/ProfileStatus.jsx
+++ src/componetnts/profile/ProfileleInfo/ProfileStatus.jsx
@@ -2,22 +2,32 @@ import React from "react";
 
 class ProfileStatus extends React.Component {
     state = {
-        editMode: false
+        editMode: false,
+        title: 'yo'
     }
-    activateEditMode(){
 
+    activateEditMode() {
+        this.setState({
+            editMode: true,
+        })
     }
+    deactivateEditMode() {
+        this.setState({
+            editMode: false,
+        })
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={() =>{alert('hey')}}>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input value={this.props.status}/>
+                    <input autoFocus={true}  onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
                 </div>
                 }
             </div>
